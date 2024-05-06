9a3a3a0d856b6caa1e805a92995f214e7982dde9
Diego Silva <diegosilvacba2017@gmail.com>
2024-05-06 15:37:07 -0300

    class transação

--- sistema.py
+++ sistema.py
@@ -1,3 +1,5 @@
+import textwrap
+from abc import ABC, abstractclassmethod, abstractproperty
 from datetime import datetime
 
 class Cliente:
@@ -124,3 +126,14 @@ class Historico:
                 "data": datetime.now().strftime("%d-%m-%Y %H:%M:%s"),
             }
         )    
+
+class Transacao(ABC):
+    @property
+    @abstractproperty
+    def valor(self):
+        pass
+
+    @abstractclassmethod
+    def registrar(self, conta):
+        pass
+
