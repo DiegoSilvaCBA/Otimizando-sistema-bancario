6adae4aa91062fa83e738bf8d83812a2527b895a
Diego Silva <diegosilvacba2017@gmail.com>
2024-05-06 15:38:05 -0300

    class saque

--- sistema.py
+++ sistema.py
@@ -1,5 +1,5 @@
 import textwrap
-from abc import ABC, abstractclassmethod, abstractproperty
+from abc import ABC, abstractclassmethod, abstractproperty 
 from datetime import datetime
 
 class Cliente:
@@ -137,3 +137,16 @@ class Transacao(ABC):
     def registrar(self, conta):
         pass
 
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
